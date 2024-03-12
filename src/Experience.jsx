0072231fc56e84e59583efec9315c3fd358dade7
S0m1326 <orrego.sebastian@correounivalle.edu.co>
2024-03-12 13:53:12 -0500

    Objetos primitivos con distintos materiales implementados

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,9 +4,35 @@ import {useRef} from "react";
 
 const Experience = () => {
     const boxRef = useRef(null);
+    const boxRef2 = useRef(null);
+    const boxRef3 = useRef(null);
+    const boxRef4 = useRef(null);
+    const boxRef5 = useRef(null);
+    const boxRef6 = useRef(null);
+    const boxRef7 = useRef(null);
+    const boxRef8 = useRef(null);
+
 
     useFrame((state, delta)=>{
-        boxRef.current.rotation.x += 1 * delta;
+        //boxRef.current.rotation.x += 1 * delta;
+        boxRef.current.position.x = -10
+        
+        boxRef2.current.position.x = -10
+        boxRef2.current.position.y = 5
+        
+        boxRef3.current.position.x = -5
+        
+        boxRef4.current.position.x = -5
+        boxRef4.current.position.y = 5
+        
+        boxRef5.current.position.y = 0
+        
+        boxRef6.current.position.y = 5
+        
+        boxRef7.current.position.x = 5
+        
+        /*boxRef8.current.position.x = 5
+        boxRef8.current.position.y = 5*/
     })
 
     return (
@@ -15,13 +41,42 @@ const Experience = () => {
             <directionalLight position={[10, 10, 5]} intensity={2} />
             <OrbitControls makeDefault />
         <mesh ref={boxRef}>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color="purple" />
+                <coneGeometry args={[2, 4, 32]} />
+                <meshStandardMaterial color="blue"/>
+            </mesh>
+        <mesh ref={boxRef2}>
+                <coneGeometry args={[2, 4, 32]} />
+                <meshPhongMaterial/>
+            </mesh>
+        <mesh ref={boxRef3}>
+            <coneGeometry args={[2, 4, 32]} /> 
+                <meshLambertMaterial color="red" />
+            </mesh>
+        <mesh ref={boxRef4}>
+                <coneGeometry args={[2, 4, 32]} />
+                <meshMatcapMaterial color="white" />
+            </mesh>
+        <mesh ref={boxRef5}>
+                <coneGeometry args={[2, 4, 32]} />
+                <meshNormalMaterial color="black" />
+            </mesh>
+        <mesh ref={boxRef6}>
+                <coneGeometry args={[2, 4, 32]} />
+                <meshPhysicalMaterial color="yellow" />
+            </mesh>
+        <mesh ref={boxRef7}>
+                <coneGeometry args={[2, 4, 32]} />
+                <meshToonMaterial color="brown" />
             </mesh>
         </>
 
     )
 }
+
+/*<mesh ref={boxRef2}>
+                <coneGeometry args={[2, 4, 32]} />
+                <meshDistanceMaterial/>
+            </mesh>*/
 
 export default Experience;
 
